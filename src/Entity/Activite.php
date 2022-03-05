78926bf6c12c937878886df0033d65ac0f020938
ceyssat <ceyssat.franck@gmail.com>
2022-03-05 18:57:02 +0100

    ajout entité Activite

---
+++ src/Entity/Activite.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActiviteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActiviteRepository::class)]
+class Activite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 36)]
+    private $activiteId;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $slug;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $titre;
+
+    #[ORM\Column(type: 'string', length: 250)]
+    private $description;
+
+    #[ORM\Column(type: 'text')]
+    private $image;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateDebut;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateFin;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateCreation;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateModification;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getActiviteId(): ?string
+    {
+        return $this->activiteId;
+    }
+
+    public function setActiviteId(string $activiteId): self
+    {
+        $this->activiteId = $activiteId;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getDateModification(): ?\DateTimeInterface
+    {
+        return $this->dateModification;
+    }
+
+    public function setDateModification(\DateTimeInterface $dateModification): self
+    {
+        $this->dateModification = $dateModification;
+
+        return $this;
+    }
+}
