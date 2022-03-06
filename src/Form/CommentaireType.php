9990360448c01ecdd444e76e360e4f6e84b2646c
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-03-06 17:28:05 +0100

    Ajoute la base de Commentaire

---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
