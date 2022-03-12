b8d8831e4e213d657568aab46264b7656430c61a
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-03-12 18:19:20 +0100

    Modifie la route de la pâge d'accueil

--- src/Controller/Site/IndexController.php
+++ src/Controller/Site/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Site;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/index', name: 'app_index')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('site/activite/index.html.twig', [
